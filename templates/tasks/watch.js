33524719030f03bafd83302b37a9ecf390949660
dielduarte <magdiel.kesller@gmail.com>
2016-03-29 16:41:30 -0300

    corrigir caminho do watch de css close #6

--- templates/tasks/watch.js
+++ templates/tasks/watch.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 
 gulp.task('watch', function () {
   gulp.watch('app/**/*.js', ['js', 'refresh']);
-  gulp.watch('app/assets/scss/**/*.scss', ['css', 'refresh']);
+  gulp.watch('app/assets/styles/**/*.scss', ['css', 'refresh']);
   gulp.watch('app/**/*.jade', ['jade', 'refresh']);
 });
